a17011d94710f13f9c6549bbf5f85e615e3b515b
ali <abadakhshan@gmail.com>
2020-08-20 11:34:08 +0430

    Upgrade to Narik 4

--- src/app/modules/share/general-list/general-list.component.ts
+++ src/app/modules/share/general-list/general-list.component.ts
@@ -1,12 +1,10 @@
 import { NarikUiListForm } from "@narik/ui-lib";
 import { Component, Injector } from "@angular/core";
-import { NarikListUi } from "../../../templates/template.decorator";
 import { DynamicForm } from "@narik/core";
 
 @DynamicForm("GeneralListComponent")
-@NarikListUi()
 @Component({
-  templateUrl: "general-list.component.html"
+  templateUrl: "general-list.component.html",
 })
 export class GeneralListComponent extends NarikUiListForm<any> {
   constructor(injector: Injector) {
